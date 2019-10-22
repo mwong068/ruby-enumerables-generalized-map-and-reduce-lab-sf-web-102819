2d2e4a16254c0065c208ad29746e4a713faff9d7
Megan Wong <mwong068@gmail.com>
2019-10-22 20:11:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,14 @@
-(map([array]){|n| n * -1})
+def map(array)
+  results = []
+  i = 0 
+  while i < array.length do
+    yield
+  i += 1 
+  end
+  return results
+end
+
+(map(array){|n| n * -1})
 (map(dune){|n| n})
-(map([1, 2, 3, -9]){|n| n * 2})
+(map(array){|n| n * 2})
+(map(array){|n| n * n})

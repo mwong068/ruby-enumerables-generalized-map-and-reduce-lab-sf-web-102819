df8071bcba28108c0556c0cf7a0cf41c419bf7bb
Megan Wong <mwong068@gmail.com>
2019-10-22 20:47:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,9 +12,9 @@ def reduce(array, starting = nil)
   if starting
     total = starting
     i = 0
-  end
   else
-  
+    i = 0 
+  end
     while i < array.length do
       total += (yield(array[i]).to_i)
     i += 1
